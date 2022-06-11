0417e132e5298a3f145e604989d2dbf586723053
baskaran2003 <105935321+baskaran2003@users.noreply.github.com>
2022-06-11 08:45:05 -0700

    Add files via upload

---
+++ link.js
@@ -0,0 +1,9 @@
+let button=document.querySelector(".navbar"),
+    links=document.querySelector(".nav-link");
+
+    button.addEventListener("click",()=>{
+        links.classList.toggle("display");
+    })
+    
+        
+    
